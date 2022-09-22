3fe8ed7cd8758cbad0a83c9aeb1adcb90938df33
killerkalamari <github@kidsquid.com>
2022-09-21 23:43:00 -0700

    Don't attempt to take screenshot if USB was never connected
    Bug: Still crashes if screenshot is taken after USB is unplugged

--- src/kbd.c
+++ src/kbd.c
@@ -18,14 +18,6 @@ You should have received a copy of the GNU General Public License
 along with Klondike Solitaire.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
-How to take an in-game screenshot:
-1. Run: fxlink -iw
-2. Connect the calculator via USB then press EXIT
-	(don't press F1 - F3)
-3. Press "7" in the game
-*/
-
 #include <gint/keyboard.h>
 #include <gint/usb-ff-bulk.h>
 #include "move.h"
@@ -48,6 +40,18 @@ void kbd_init(void)
 {
 	usb_interface_t const *interfaces[] = { &usb_ff_bulk, NULL };
 	usb_open(interfaces, GINT_CALL_NULL);
+}
+
+/*
+How to take an in-game screenshot:
+1. Run: fxlink -iw
+2. Connect the calculator via USB then press EXIT (don't press F1 - F3)
+3. Press "7" in the game
+*/
+static void take_screenshot(void)
+{
+	if (usb_is_open())
+		usb_fxlink_screenshot(1);
 }
 
 command_t kbd_game_input(void)
@@ -92,7 +96,7 @@ command_t kbd_game_input(void)
 			return COMMAND_NONE;
 		case KEY_7:
 			// Take screenshot
-			usb_fxlink_screenshot(1);
+			take_screenshot();
 			break;
 		}
 	}
@@ -114,7 +118,7 @@ command_t kbd_options_input(void)
 			return COMMAND_HELP_FR;
 		case KEY_7:
 			// Take screenshot
-			usb_fxlink_screenshot(1);
+			take_screenshot();
 			break;
 		case KEY_EXIT:
 			// Exit options menu
@@ -129,7 +133,7 @@ void kbd_help_input(void)
 		switch (getkey_opt(0xDC, NULL).key) {
 		case KEY_7:
 			// Take screenshot
-			usb_fxlink_screenshot(1);
+			take_screenshot();
 			break;
 		case KEY_EXIT:
 			// Exit help
